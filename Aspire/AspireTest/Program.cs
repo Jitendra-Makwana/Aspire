fb4ab811dfb0f58a92311907e1327985691e5629
Jitendra Makwana <49226078+Jitendra-Makwana@users.noreply.github.com>
2024-07-26 17:46:50 +0530

    Adding Metrics and Traces

--- Aspire/AspireTest/Program.cs
+++ Aspire/AspireTest/Program.cs
@@ -1,21 +1,50 @@
-using OpenTelemetry.Exporter;
 using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Logging.ClearProviders();
-builder.Logging.AddOpenTelemetry(x => 
+// builder.Logging.ClearProviders();
+builder.Logging.AddOpenTelemetry(x =>
     {
         x.IncludeScopes = true;
         x.IncludeFormattedMessage = true;
-        x.AddOtlpExporter(a => 
-            {
-                a.Endpoint = new Uri("http://localhost:5341/ingest/otlp/v1/logs");
-                a.Protocol = OtlpExportProtocol.HttpProtobuf;
-                a.Headers = "X-Seq-ApiKey=SlW8CeWtWwWZibbEfz9Q";
-            }
-        );
-});
+        // x.AddConsoleExporter();
+
+        // x.AddOtlpExporter(a =>
+        //     {
+        //         a.Endpoint = new Uri("http://localhost:5341/ingest/otlp/v1/logs");
+        //         a.Protocol = OtlpExportProtocol.HttpProtobuf;
+        //         a.Headers = "X-Seq-ApiKey=Khfqg02u2F9DBpCiAoKW";
+        //     }
+        // );
+
+    });
+
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(x =>
+    {
+        x.AddRuntimeInstrumentation()
+            .AddMeter("AspireTest");
+    })
+    .WithTracing(x =>
+    {
+
+        if (builder.Environment.IsDevelopment())
+        {
+            x.SetSampler<AlwaysOnSampler>();
+        }
+        x.AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation();
+    });
+var useOtlpExporter = !string.IsNullOrWhiteSpace(builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"]);
+
+if (useOtlpExporter)
+{
+    builder.Services.Configure<OpenTelemetryLoggerOptions>(logging => logging.AddOtlpExporter());
+    builder.Services.ConfigureOpenTelemetryMeterProvider(metrics => metrics.AddOtlpExporter());
+    builder.Services.ConfigureOpenTelemetryTracerProvider(traces => traces.AddOtlpExporter());
+}
 
 var app = builder.Build();
 
@@ -25,21 +54,20 @@ if (app.Environment.IsDevelopment())
     app.UseHttpsRedirection();
 }
 
-
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
 app.MapGet("/", () =>
-{    
+{
     return "Hello there!";
 });
 
 app.MapGet("/weatherforecast", (string city, int days, ILogger<Program> _logger) =>
 {
     _logger.LogInformation("Entered into controller");
-    var forecast =  Enumerable.Range(1, days).Select(index =>
+    var forecast = Enumerable.Range(1, days).Select(index =>
         new WeatherForecast
         (
             city,
